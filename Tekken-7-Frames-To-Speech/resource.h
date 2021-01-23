4a703fc3c8704afabef2b5ec7b03f52c58ec0a81
AP <ParadiseAigo@hotmail.com>
2021-01-23 19:33:15 +0100

    visual studio added wav's as a resource

---
+++ Tekken-7-Frames-To-Speech/resource.h
@@ -0,0 +1,30 @@
+//{{NO_DEPENDENCIES}}
+// Microsoft Visual C++ generated include file.
+// Used by Tekken-7-Frames-To-Speech.rc
+//
+#define IDR_WAVE10                      101
+#define IDR_WAVE11                      102
+#define IDR_WAVE12                      103
+#define IDR_WAVE13                      104
+#define IDR_WAVE14                      105
+#define IDR_WAVE15                      106
+#define IDR_WAVE16                      107
+#define IDR_WAVE17                      108
+#define IDR_WAVE18                      109
+#define IDR_WAVE19                      110
+#define IDR_WAVE20                      111
+#define IDR_WAVE21                      112
+#define IDR_WAVE22                      113
+#define IDR_WAVE23                      114
+#define IDR_WAVE24                      115
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NEXT_RESOURCE_VALUE        116
+#define _APS_NEXT_COMMAND_VALUE         40001
+#define _APS_NEXT_CONTROL_VALUE         1001
+#define _APS_NEXT_SYMED_VALUE           101
+#endif
+#endif
