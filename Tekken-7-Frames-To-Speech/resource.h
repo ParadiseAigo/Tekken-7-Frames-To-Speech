a9fef360f830effbe6bb53f88cb3629983a35e2f
AP <ParadiseAigo@hotmail.com>
2021-01-26 23:39:05 +0100

    added exe icon

--- Tekken-7-Frames-To-Speech/resource.h
+++ Tekken-7-Frames-To-Speech/resource.h
@@ -17,12 +17,13 @@
 #define IDR_WAVE22                      113
 #define IDR_WAVE23                      114
 #define IDR_WAVE24                      115
+#define IDI_ICON1                       116
 
 // Next default values for new objects
 // 
 #ifdef APSTUDIO_INVOKED
 #ifndef APSTUDIO_READONLY_SYMBOLS
-#define _APS_NEXT_RESOURCE_VALUE        116
+#define _APS_NEXT_RESOURCE_VALUE        117
 #define _APS_NEXT_COMMAND_VALUE         40001
 #define _APS_NEXT_CONTROL_VALUE         1001
 #define _APS_NEXT_SYMED_VALUE           101
